08c820eda41c3efe327640961dbd05c8ddc905f1
Timothy Johnson <tjohnson.acts1422@gmail.com>
2020-07-24 22:37:18 -0400

    Added a game function; STATUS: IN PROGRESS; Cannot return a result variable from the singleRound fx

--- rps.js
+++ rps.js
@@ -1,46 +1,69 @@
-function game() {
-    singleRound(); 
+function game() { 
+    let result; 
+    let playerTotalScore = 0; 
+    let compTotalScore = 0;  
+    let numOfRounds = parseInt(prompt(`How many rounds do you want to play?`)); 
+    if (!isNaN(numOfRounds)) {
+        for (let i = 0; i < numOfRounds; i++) {
+            console.log(`This is Round ${i+1}... begin!`);
+            // singleRound(playerDecision = prompt('Choose: Rock, Paper or Scissors'), computerPlay());
+            singleRound(); 
+            if (result.contains('You win')) {
+                 playerTotalScore += 1; 
+            } else if (result.contains('You lose')) {
+                playerTotalScore -= 1;
+                compTotalScore += 1; 
+            } else {
+                // singleRound(playerDecision = prompt('Choose: Rock, Paper or Scissors'), computerPlay()); 
+                singleRound(); 
+            }
+        }
+    } 
+    // return function() { return result; }
+    console.log(`After ${numOfRounds} rounds, the winner is...`);
+    console.log("winner!")
 }
 
 
-function singleRound(playerDecision, computerDecision) {
-   let result;
-    if (playerDecision === 'Rock') {
+var singleRound = function (playerDecision = prompt('Choose: Rock, Paper or Scissors'), computerDecision) {
+    // let result;
+    if (playerDecision.toLowerCase() === 'rock') {
         switch (computerDecision) {
-            case 'Paper': result = `You lose, ${computerDecision} beats ${playerDecision}.`;
+            case 'Paper': result = `You lose, ${computerDecision} beats ${playerDecision}. You lost one point!`;
             break; 
-            case 'Scissors': result = `You win, ${playerDecision} beats ${computerDecision}.`;
+            case 'Scissors': result = `You win, ${playerDecision} beats ${computerDecision}. You earned one point!`;
             break; 
-            default: result = `Choose again, ${computerDecision} matches ${playerDecision}.`;
+            default: result = `Choose again, ${computerDecision} matches ${playerDecision}. You earned no points...yet!`;
             break; 
         }
-        console.log(result); 
-     } else if (playerDecision === 'Paper') {
+    return result; 
+    } else if (playerDecision.toLowerCase() === 'paper') {
         switch (computerDecision) {
-            case 'Rock': result = `You win, ${playerDecision} beats ${computerDecision}.`;
+            case 'Rock': result = `You win, ${playerDecision} beats ${computerDecision}. You earned one point!`;
             break; 
-            case 'Scissors': result = `You lose, ${computerDecision} beats ${playerDecision}.`;
+            case 'Scissors': result = `You lose, ${computerDecision} beats ${playerDecision}. You lost one point!`;
             break; 
-            default: result = `Choose again, ${computerDecision} matches ${playerDecision}.`;
+            default: result = `Choose again, ${computerDecision} matches ${playerDecision}. You earned no points...yet!`;
             break;
         } 
-        console.log(result); 
-    } else if (playerDecision === 'Scissors') {
+        return result;  
+    } else if (playerDecision.toLowerCase() === 'scissors') {
         switch (computerDecision) {
-            case 'Paper': result = `You win, ${playerDecision} beats ${computerDecision}.`;
+            case 'Paper': result = `You win, ${playerDecision} beats ${computerDecision}. You earned one point!`;
             break; 
-            case 'Rock': result = `You lose, ${computerDecision} beats ${playerDecision}.`;
+            case 'Rock': result = `You lose, ${computerDecision} beats ${playerDecision}. You lost one point!`;
             break; 
-            default: result = `Choose again, ${computerDecision} matches ${playerDecision}.`;
+            default: result = `Choose again, ${computerDecision} matches ${playerDecision}. You earned no points...yet!`;
             break;  
         };
-        console.log(result);
+        return result;  
     } else {
         alert(`Your selection of ${playerDecision} is not valid. Please try again. `); 
-        singleRound(playerDecision = prompt('Choose: Rock, Paper or Scissors'), computerPlay()); 
+        // singleRound(playerDecision = prompt('Choose: Rock, Paper or Scissors'), computerPlay()); 
+        singleRound(), computerPlay(); 
     }
+    // return 
 }
-singleRound(playerDecision = prompt('Choose: Rock, Paper or Scissors'), computerPlay()); 
 
 function computerPlay() {
     let choices = ['Rock','Paper', 'Scissors']; 
@@ -49,3 +72,5 @@ function computerPlay() {
     console.log(decision); 
     return decision; 
 }
+
+game(); 
