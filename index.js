359c9e84766a66e0a2cba8247d8cad62feb3948c
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2023-07-31 11:12:52 +0300

    Create new index.js file to refactor RPS

---
+++ index.js
@@ -0,0 +1,11 @@
+//create a startGame function
+// get input from user of how many rounds to play (max of 9?)
+// parse the input
+// pass parsed input to main game function
+// intialize a round function with parsed input
+// get round choice from the user
+// get random choice from the computer
+// determine winner of the round
+// print that to the console
+// check if computer/user has won enough rounds to win the game (checkWin)
+// 
