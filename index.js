ff1d3f51eb5664f3ee76b02d874f05dd5158d9a5
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2023-07-31 11:56:29 +0300

    Update new index.js file with some rules of RPS

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 //create a startGame function
-// get input from user of how many rounds to play (max of 9?)
-// parse the input
-// pass parsed input to main game function
-// intialize a round function with parsed input
-// get round choice from the user
-// get random choice from the computer
-// determine winner of the round
-// print that to the console
-// check if computer/user has won enough rounds to win the game (checkWin)
-// 
+function newGame() {
+    // get input from user of how many rounds to play (max of 9)
+    let numOfROunds = () => {
+        // parse the input
+        let rounds = parseInt(prompt(`How many rounds would you like to play?`)); 
+        return rounds; 
+    }; 
+    // pass parsed input to main game function
+    // intialize a startRound or playRound function with parsed input
+
+    // get round choice from the user
+    // get round choice from the computer (random)
+    // determine winner of the round
+    // print that to the console
+    // check if computer/user has won enough rounds to win the game (checkWin)
+    // announce winner of the whole game
+}
+// prompt user to play again?
+    function playAgain() {
+
+    }
+
+    function getComputerChoice() {
+        // create an array of string options for RPS
+        let options = ['rock', 'paper', 'scissors']; 
+        // get a random number between 0 and 2
+        let choice = Math.floor(Math.random() * options.length); 
+        //return the item at the index of the choice
+        return options[choice]; 
+    }
+
+    function playRound(playerChoice, computerChoice) {
+        // get Player choice
+        playerChoice = prompt(`Rock, Paper or Scissors?`).toLowerCase(); 
+        // get computer choice 
+        computerChoice = getComputerChoice(); 
+        // determine winner
+        getRoundWinner(playerChoice, computerChoice); 
+        // return winner
+        // update score
+    }
+    function getRoundWinner(player, computer) {
+        
+    }
+    newGame(); 
+    // console.log(getComputerChoice()); 
