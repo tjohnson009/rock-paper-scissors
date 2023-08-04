67eccf1b634f840374fb738a2516be4376e4bf5f
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2023-08-04 14:59:15 +0300

    Fix issue with infinite callback on prompting user input

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ function newGame() {
         // get a random number between 0 and 2
         let choice = Math.floor(Math.random() * options.length); 
         //return the item at the index of the choice
+        console.log(`Computer chooses ${options[choice]}`); 
         return options[choice]; 
     }
 
-    function playRound(playerChoice, computerChoice) {
+    function playRound() {
         // get Player choice
         // playerChoice = getPlayerChoice(); 
         // get computer choice 
@@ -40,32 +41,31 @@ function newGame() {
     
         // update score
     }
-    
+
     function getPlayerChoice() {
         //intialize return value playerChoice
         let playerChoice; 
         //prompt user for a choice
         let input = prompt(`Rock, Paper or Scissors?`).toLowerCase(); 
-        // set playerChoice or reprompt the user for a valid input
-        input === 'rock' ? playerChoice = 'rock' 
-        : input === 'paper' ? playerChoice = 'paper' 
-        : input === 'scissors' ? playerChoice = 'scissors' 
-        : `getPlayerChoice()`;
-        console.log(`Player choice confirmed.`)
-        return playerChoice; 
-    }
+        // set playerChoice or reprompt the user for a valid input - PERHAPS USE A PROPER IF STATEMENT HERE
+        return input == 'rock' ? playerChoice = 'rock' : 
+        input == 'paper' ? playerChoice = 'paper' :
+        input == 'scissors' ? playerChoice = 'scissors' :
+        getPlayerChoice(); // call the function again to ensure that 
+    }; 
 
     function getRoundWinner(player, computer) {
+        let result = ``; 
         if (player === 'rock') {
-            computer === 'rock' ? `It's a tie` : 'paper' ? `Computer wins!` : `Player wins`; 
-            console.log('player chose rock'); 
+            console.log('Player chose rock'); 
+            return computer === 'rock' ? console.log(`It's a tie`) : computer === 'paper' ? console.log(`Computer wins!`) : console.log(`Player wins`); 
         } else if (player === 'paper') {
-
+            // console.log('paper wins'); 
         } else {
-
+            // console.log('scissors wins'); 
         }; 
     }
     // newGame(); 
     // console.log(getComputerChoice()); 
     // getPlayerChoice(); 
-    playRound(getPlayerChoice(), getComputerChoice()); 
+    playRound(); 
